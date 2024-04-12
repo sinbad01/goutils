f60560d6083026a4cbb934c53827e70dd4fb94fb
sinbad <sinbad01@163.com>
2024-04-12 11:36:09 +0800

    简化代码

--- hash/md5.go
+++ hash/md5.go
@@ -3,7 +3,6 @@ package hash
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -12,17 +11,13 @@ import (
 func FileMD5(filename string) (string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
-		return "", errors.New(
-			fmt.Sprintf("md5.go hash.FileMD5 os open error %v", err),
-		)
+		return "", fmt.Errorf("md5.go hash.FileMD5 os open error %v", err)
 	}
 
 	md5 := md5.New()
 	_, err = io.Copy(md5, file)
 	if err != nil {
-		return "", errors.New(
-			fmt.Sprintf("md5.go hash.FileMD5 os copy error %v", err),
-		)
+		return "", fmt.Errorf("md5.go hash.FileMD5 os copy error %v", err)
 	}
 
 	return hex.EncodeToString(md5.Sum(nil)), nil
