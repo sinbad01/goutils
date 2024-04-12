c44777dea570486331a7808f50e70157cc231898
sinbad <sinbad01@163.com>
2024-04-12 11:08:52 +0800

    Add md5 functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sinbad01/goutils
+
+go 1.21.5
---
+++ hash/md5.go
@@ -0,0 +1,35 @@
+package hash
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func FileMD5(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", errors.New(
+			fmt.Sprintf("md5.go hash.FileMD5 os open error %v", err)
+		)
+	}
+
+	md5 := md5.New()
+	_, err = io.Copy(md5, file)
+	if err != nil {
+		return "", errors.New(
+			fmt.Sprintf("md5.go hash.FileMD5 os copy error %v", err)
+		)
+	}
+
+	return hex.EncodeToString(md5.Sum(nil)), nil
+}
+
+func StringMD5(s string) string {
+	md5 := md5.New()
+	md5.Write([]byte(s))
+	return hex.EncodeToString(md5.Sum(nil))
+}
